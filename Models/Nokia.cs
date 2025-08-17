751bf30f7475b7d38cf120c7aca2495a619d9124
LucasBarbosads <108785515+LucasBarbosads@users.noreply.github.com>
2025-08-17 16:09:43 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -7,6 +7,8 @@ namespace DesafioPOO.Models
         { }
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         { }
+
+        //Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Smartphone Nokia.");
