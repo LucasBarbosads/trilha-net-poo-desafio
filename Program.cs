b4b8a7412d5374a3341ef10f1c1bcfebf317fe5b
Lucas Barbosa <lucas09942@gmail.com>
2025-08-17 16:08:16 -0300

    criação do método instalarAplicativo nas classes Nokia e Iphone que foram herdados da classe abstract Smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Usando o smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "2536486", modelo: "xr-90", imei: "2563486", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("----------------------------------------");
+
+Console.WriteLine("Usando o smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "9686413", modelo: "Iphone x", imei: "578864", memoria: 525);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
